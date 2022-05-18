f3af472fc2c3cedb68a7f0630495249dcc6daaeb
Mustafa Yafes Akbaş <62028911+might4@users.noreply.github.com>
2022-05-18 19:44:35 +0300

    tryn to solv somthnn

--- src/main/Main.java
+++ src/main/Main.java
@@ -217,6 +217,9 @@ public class Main<second> extends Canvas implements Runnable{
                 g.drawImage(background, 0, 0, getWidth(), getHeight(), null);
                 //COIN ADDITIONS
                 g.drawImage(Main.coin.getBufferedImage(),25,25,60,60,null);
+                //for rendering blocks only if show death screen is false
+                if(playing) g.translate(cam.getX(),cam.getY());
+                if(playing) handler.render(g);
                 g.setColor(Color.WHITE);
                 g.setFont(new Font("Pixel NES",Font.PLAIN,45));
                 g.drawString(":" + coins, 80, 80);
@@ -230,9 +233,6 @@ public class Main<second> extends Canvas implements Runnable{
                 g.drawImage(Main.player[1].getBufferedImage(),getWidth()-172,23,60,60,null);
                 g.setFont(new Font("Pixel NES",Font.PLAIN,45));
                 g.drawString("x" + lives, getWidth()-100, 80);
-                //for rendering blocks only if show death screen is false
-                if(playing) g.translate(cam.getX(),cam.getY());
-                if(playing) handler.render(g);
 
             }
             //
